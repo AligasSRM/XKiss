5ffcede963813953e2d02a618da170e3b14a4a20
AligasSRM <srourr.ali73@gmail.com>
2026-09-25 15:28:18 +0300

    Add XKiss video data system

---
+++ video-data.js
@@ -0,0 +1,36 @@
+const XKISS_VIDEOS = {
+
+  "video-001": {
+
+    id: "video-001",
+
+    title: "XKiss Test Video",
+
+    creator: "XKiss Creator",
+
+    duration: "00:04",
+
+    tags: [
+      "XKiss",
+      "Video",
+      "Test"
+    ],
+
+    sources: {
+
+      "340p": "",
+
+      "460p": "",
+
+      "720p":
+        "https://aligassrm.github.io/XKiss/20260923_234200-3.mp4",
+
+      "1080p": ""
+
+    },
+
+    views: 0
+
+  }
+
+};
