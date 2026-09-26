532913246b6fd56888009a9e721820510d566a72
AligasSRM <srourr.ali73@gmail.com>
2026-09-26 03:58:52 +0300

    Add XKiss 15.23 runtime control layer

---
+++ player/runtime/player-runtime-control-integration.js
@@ -0,0 +1,3 @@
+import{controlXKissPlayerRuntime}from"./player-runtime-control-core.js";
+export function validateXKissPlayerRuntimeControlIntegration(){return{ok:true,stage:"15.23",integrationConnected:true};}
+export function executeXKissPlayerRuntimeControl(request={}){return controlXKissPlayerRuntime(request);}
